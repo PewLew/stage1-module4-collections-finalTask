2e3c1358db83c269c302c4cd9d843c087e075cc6
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2024-01-16 00:37:26 +0600

    sdf

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
-        return null;
+        List<String> myList = new ArrayList<>();
+        for(var e : projects.entrySet()){
+            for(var value : e.getValue()){
+                if(value.equals(developer))
+                    myList.add(e.getKey());
+            }
+        }
+
+        String a;
+        for(int i = 0; i < myList.size()-1; i++){
+            for(int j = 0; j< myList.size()-1; j++){
+                if(myList.get(j).length() < myList.get(j+1).length()){
+                    a = myList.get(j);
+                    myList.set(j, myList.get(j+1));
+                    myList.set(j+1, a);
+                }
+            }
+        }
+
+        return myList;
     }
 }
